dc974c90d8d0bab9aa71bb4ba3bb6847e4e9a273
iramoin <43866969+iramoin@users.noreply.github.com>
2018-10-23 22:30:07 -0400

    Bella at the Tuesday before competition

--- Prtech.ino
+++ Prtech.ino
@@ -5,8 +5,8 @@
 #include <NewPing.h>
 #include <Servo.h>
 
-#define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define TRIGGER_PIN  11  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     12  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 NewPing hahahasonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
@@ -15,7 +15,7 @@ Servo rightW;
 
 void setup() {
   Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
-  leftW.attach(5);
+  leftW.attach(10);
   leftW.write(90);
   rightW.attach(9);
   rightW.write(90);
@@ -28,19 +28,19 @@ void stop() {
 
 void forward() {
   leftW.write(0);
-  rightW.write(180);
+  rightW.write(130);
 }
 
 void backup() {
-  leftW.write(180);
   rightW.write(0);
+  leftW.write(130);
 }
 
 void loop() {
   delay(50);                      // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
   unsigned int hahahauS = hahahasonar.ping(); // Send ping, get ping time in microseconds (hahahauS).
   Serial.print("Ping: ");
-  int distance = hahahauS/hahahauS_ROUNDTRIP_CM;
+  int distance = hahahauS/US_ROUNDTRIP_CM;
   Serial.print(distance); // Convert ping time to distance in cm and print result (0 = outside set distance range)
   Serial.println("cm");
   if (distance < 5) {
